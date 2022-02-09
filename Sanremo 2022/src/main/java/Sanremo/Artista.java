6636a2efbeeba767c8b807d6af50179f7f139648
vol4ainf-12 <vol4ainf-12@vol12.colamonico.itc>
2022-02-09 13:10:01 +0100

    09/02/2022 12:10

---
+++ Sanremo 2022/src/main/java/Sanremo/Artista.java
@@ -0,0 +1,36 @@
+package Sanremo;
+
+public class Artista {
+	
+	String nome, canzone, immagine;
+
+	public Artista() {
+		super();
+		
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCanzone() {
+		return canzone;
+	}
+
+	public void setCanzone(String canzone) {
+		this.canzone = canzone;
+	}
+
+	public String getImmagine() {
+		return immagine;
+	}
+
+	public void setImmagine(String immagine) {
+		this.immagine = immagine;
+	}
+
+}
